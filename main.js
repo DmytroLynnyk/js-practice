8acf00626ad7bad2a35ac1271c3230412f2ced7e
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-11 22:35:24 +0200

    Abstract 07

--- main.js
+++ main.js
@@ -4791,7 +4791,7 @@
 //   textInput.value = "";
 // });
 
-// ✅ ✅ Mentor practice 05
+// ✅ ✅ Mentor practice 06
 
 // ✅ Task 01
 // const makeShef = function (name) {
@@ -4834,3 +4834,217 @@
 // console.log(fn("Number"));
 
 // ✅ Task 04
+
+// ✅ ✅ ✅ Module 07
+
+// ✅ ✅ Abstract 07
+
+// ✅ Task 01
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+// parent.addEventListener("click", () => {
+//   console.log("Parent click handler");
+// });
+// child.addEventListener("click", () => {
+//   console.log("Child click handler");
+// });
+// descendant.addEventListener("click", () => {
+//   console.log("Descendant click handler");
+// });
+
+// ✅ Task 02
+// const parent = document.querySelector("#parent");
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+
+// ✅ Task 03
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+// parent.addEventListener("click", () => {
+//   alert(
+//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+// child.addEventListener("click", () => {
+//   alert(
+//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+// descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Descendant click handler");
+// });
+
+// ✅ Task 04
+// const colorPalette = document.querySelector(".color-palette");
+// const output = document.querySelector(".output");
+// colorPalette.addEventListener("click", selectColor);
+// // This is where delegation «magic» happens
+// function selectColor(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     return;
+//   }
+//   const selectedColor = event.target.dataset.color;
+//   output.textContent = `Selected color: ${selectedColor}`;
+//   output.style.color = selectedColor;
+// }
+// // Some helper functions to render palette items
+// createPaletteItems();
+// function createPaletteItems() {
+//   const items = [];
+//   for (let i = 0; i < 60; i++) {
+//     const color = getRandomHexColor();
+//     const item = document.createElement("button");
+//     item.type = "button";
+//     item.dataset.color = color;
+//     item.style.backgroundColor = color;
+//     item.classList.add("item");
+//     items.push(item);
+//   }
+//   colorPalette.append(...items);
+// }
+// function getRandomHexColor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+// ✅ Task 05
+// const output = document.querySelector(".output");
+// let scrollEventCounter = 0;
+// document.addEventListener("scroll", () => {
+//   scrollEventCounter += 1;
+//   output.textContent = scrollEventCounter;
+// });
+
+// ✅ Task 06
+// const result = _.add(2, 3);
+// console.log(result); // 5
+
+// ✅ Task 07
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+//   }, 300)
+// );
+
+// ✅ Task 08
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+// };
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
+
+// ✅ Task 08
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     console.log("Scroll handler call after 300ms pause");
+//   }, 300)
+// );
+
+// ✅ Task 09
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const debouncedOutput = document.querySelector(".output.debounced");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+//   debounced: 0,
+// };
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     eventCounter.debounced += 1;
+//     debouncedOutput.textContent = eventCounter.debounced;
+//   }, 300)
+// );
+
+// ✅ Task 10
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(
+//     () => {
+//       console.log("Scroll handler call on every event stream start");
+//     },
+//     300,
+//     {
+//       leading: true,
+//       trailing: false,
+//     }
+//   )
+// );
+
+// ✅ Task 11
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const trailingOutput = document.querySelector(".output.trailing");
+// const leadingOutput = document.querySelector(".output.leading");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+//   trailing: 0,
+//   leading: 0,
+// };
+// // Trailing debounce
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     eventCounter.trailing += 1;
+//     trailingOutput.textContent = eventCounter.trailing;
+//   }, 300)
+// );
+// // Leading debounce
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(
+//     () => {
+//       eventCounter.leading += 1;
+//       leadingOutput.textContent = eventCounter.leading;
+//     },
+//     300,
+//     { trailing: false, leading: true }
+//   )
+// );
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
